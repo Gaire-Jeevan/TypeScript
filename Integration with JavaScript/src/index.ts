b6671f1b9a47033adaac4fd4542d346708d89560
Jeevan <gairejeevan7@gmail.com>
2024-10-01 14:50:12 +0530

    Definitely Typed Declaration Files.

--- Integration with JavaScript/src/index.ts
+++ Integration with JavaScript/src/index.ts
@@ -1,4 +1,8 @@
-import {calculateIax} from './tax'
+// npm i --save-dev @types/lodash --> to install declaration file for lodash from definitely typed github repository
+// npm i chalk --> to install chalk which provide own declaration file no need fo rit to install from definitely typed library
 
-let tax = calculateIax(1000);
-console.log(tax)
+
+
+import * as _ from 'lodash';
+
+_.clone([1, 2, 3])
