27cb120a03684e66e9595e95d3d8fc2ac8ebf344
Jeevan <gairejeevan7@gmail.com>
2024-10-02 12:39:37 +0530

    Handling Delete event.

--- React with TypeScript/reminders-app/src/components/ReminderList.tsx
+++ React with TypeScript/reminders-app/src/components/ReminderList.tsx
@@ -3,13 +3,22 @@ import Reminder from "../models/reminder";
 
 interface ReminderListProps {
   items: Reminder[];
+  onRemoveReminder: (id: number) => void;
 }
 
-const ReminderList = ({ items }: ReminderListProps) => {
+const ReminderList = ({ items, onRemoveReminder }: ReminderListProps) => {
   return (
     <ul className="list-group">
       {items.map((item) => (
-        <li key={item.id} className="list-group-item">{item.title}</li>
+        <li key={item.id} className="list-group-item">
+          {item.title}
+          <button
+            onClick={() => onRemoveReminder(item.id)}
+            className="btn btn-outline-danger mx-2 rounded-pill"
+          >
+            Delete
+          </button>
+        </li>
       ))}
     </ul>
   );
